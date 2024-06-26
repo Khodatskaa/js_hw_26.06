8c4083fa7601dd19ff5b7107d67d143084d53c6c
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-26 21:42:21 +0300

    Ex 2 was done

---
+++ 01-.js
@@ -0,0 +1,30 @@
+
+const total = 100;
+let ordered = 50;
+
+if (ordered > total) {
+console.log("Insufficient quantity in stock!");
+} else {
+console.log("Order is made.");
+}
+
+ordered = 20;
+if (ordered > total) {
+console.log("Insufficient quantity in stock!");
+} else {
+console.log("Order is made.");
+}
+
+ordered = 80;
+if (ordered > total) {
+console.log("Insufficient quantity in stock!");
+} else {
+console.log("Order is made.");
+}
+
+ordered = 130;
+if (ordered > total) {
+console.log("Insufficient quantity in stock!");
+} else {
+console.log("Order is made.");
+}
